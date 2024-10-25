c90def33689bf2a6f53535dbb1698c4d77c3c633
North Harrington <339713067@GoTVDSB.ca>
2024-10-25 09:36:06 -0400

    Add project files.

---
+++ topic 7.2 tut/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topic_7._2_tut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            //int number1;
+            //int number2;
+            //int number3;
+            //Console.WriteLine("What 3 numbers would you like me to count to?");
+            //number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("second number");
+            //number2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("third number");
+            //number3 = int.Parse(Console.ReadLine());
+
+
+
+            //Part 1 
+            //for (int i = number1; i <= number2; i = i + number3)
+            //{
+            //Console.WriteLine(i + ": " );
+
+            //}
+            //Console.ReadLine();
+
+            //Console.WriteLine("countdown");
+
+
+            //for (int i = 10; i >= 0; --i)
+            //{
+            // Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            //Console.WriteLine("countdown");
+
+
+            //Part 2
+            for (int i = -10; i < 12; i+=2)
+            {
+                Console.WriteLine(i);Console.WriteLine(i*i);
+           }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
